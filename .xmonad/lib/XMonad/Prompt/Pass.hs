9d89f54b31fbcd44e0fcc6694dce8844a641dcf3
Ondřej Súkup (mimi_vx) <mimi.vx@gmail.com>
2014-06-17 22:15:08 +0200

    coding standard of xmonad-contrib

--- .xmonad/lib/XMonad/Prompt/Pass.hs
+++ .xmonad/lib/XMonad/Prompt/Pass.hs
@@ -1,14 +1,39 @@
-------------------------------------------------------------------------------------
---          Pass Promp module based on:
---          http://blog.tarn-vedra.de/2014/05/xmonad-loves-password-store.html
-------------------------------------------------------------------------------------    
--- Module:                    XMonad.Prompt.Pass
+-----------------------------------------------------------------------------
+-- | 
+-- Module      :  XMonad.Prompt.Pass
+-- Copyright   :  (c) 2014 Ondřej Súkup
+--             :  (c) 2014 Moritz Ulrich
+-- License     :  
 --
-------------------------------------------------------------------------------------
+-- 
+-- Maintainer  :  Ondřej Súkup 
+-- Stability   :  unstable
+-- Portability :  unportable
+--
+-- Provides promt for Pass - CLI password storage
+--
+-----------------------------------------------------------------------------
 
 module XMonad.Prompt.Pass (
-        passPrompt)
-  where
+                          -- * Usage
+                          -- $usage
+                          passPrompt
+                          ) where
+-- $usage
+-- You can use this module with the following in your @~\/.xmonad\/xmonad.hs@:
+--
+-- > import XMonad.Prompt
+-- > import XMonad.Prompt.Pass
+--
+-- Then add a keybinding for 'passPrompt':
+--
+-- >   , ((modMask x .|. controlMask, xK_h), passPrompt defaultXPconfig)
+--
+-- For detailed instructions on editing your key bindings, see
+-- "XMonad.Doc.Extending#Editing_key_bindings".
+ 
+-- 
+--
 
 import System.Environment
 import System.FilePath.Find
